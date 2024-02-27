283062c37e64e5080e5186f6431d92a28cf2937a
lexneconst <songkeart@gmail.com>
2024-02-27 10:46:11 +0700

    Signed-off-by: lexneconst <songkeart@gmail.com>

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ git config --global user.name "lexneconst"
 git add .
 git add --all
 git status
-git commit -am 'carroll v1.1'
+git commit -am 'pcacrw v1.1'
 git remote add origin 'https://github.com/lexneconst/pcacrw.git'
 git push -u origin main
 
